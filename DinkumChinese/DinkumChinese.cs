2b5527e844f05c4cdbaaa777f0ecd10e245b4f1f
宵夜 <34893504+xiaoye97@users.noreply.github.com>
2023-12-03 09:53:17 +0800

    Update DinkumChinese.cs

--- DinkumChinese/DinkumChinese.cs
+++ DinkumChinese/DinkumChinese.cs
@@ -19,7 +19,7 @@ namespace DinkumChinese
     {
         public const string GUID = "xiaoye97.Dinkum.DinkumChinese";
         public const string PluginName = "DinkumChinese";
-        public const string Version = "1.19.0";
+        public const string Version = "1.20.0";
         public static DinkumChinesePlugin Inst;
 
         public static Queue<TextMeshProUGUI> waitShowTMPs = new Queue<TextMeshProUGUI>();
